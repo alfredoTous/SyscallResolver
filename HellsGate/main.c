d8750b998c805d13c7cce7d2cbae87c21465b2c2
alfredoTous <adtous@uninorte.edu.co>
2026-01-30 10:42:40 -0500

    Implement GetSSNByName, parses NTDLL and gets the requested function address, then parses raw bytes of syscall stub for SSN (HellsGate implementation)

---
+++ HellsGate/main.c
@@ -0,0 +1,91 @@
+#include <windows.h>
+#include <stdio.h>
+#include "WinStructs.h"
+
+
+HMODULE CustomGetModuleHandle(wchar_t* moduleName)
+{
+    // Get PEB by offset in gs register
+    PPEB pPeb = (PPEB)__readgsqword(0x60);
+    // Contains linked list of loaded modules
+    PPEB_LDR_DATA pLdr = pPeb->Ldr;
+
+    PLDR_DATA_TABLE_ENTRY pDte = (PLDR_DATA_TABLE_ENTRY)pLdr->InMemoryOrderModuleList.Flink;
+    PLDR_DATA_TABLE_ENTRY pStart = pDte;
+
+    while (TRUE) {
+        if (_wcsicmp(pDte->FullDllName.Buffer, moduleName) == 0) {
+            wprintf(L"[+] Found %s: 0x%p \n", pDte->FullDllName.Buffer, (HMODULE)pDte->Reserved2[0]);
+            // Return base address of module, which is the below member in official Windows struct
+            return (HMODULE)pDte->Reserved2[0];
+        }
+        // Access next node through dereference
+        pDte = *(PLDR_DATA_TABLE_ENTRY*)pDte;
+
+        // Module not found in linked list full iteration
+        if (pDte == pStart) {
+            wprintf(L"[-] Module %s not found\n");
+            return NULL;
+        }
+    }
+}
+
+
+DWORD GetSSNByName(IN HMODULE hModule, IN char* syscallName)
+{
+    BYTE* pBase = (BYTE*)hModule;
+    PIMAGE_DOS_HEADER pImgDosHdr = (PIMAGE_DOS_HEADER)pBase;
+    if (pImgDosHdr->e_magic != IMAGE_DOS_SIGNATURE) {
+        printf("[-] Failed getting PIMAGE_DOS_HEADER\n");
+        return NULL;
+    }
+    
+    PIMAGE_NT_HEADERS pImgNtHdrs = (PIMAGE_NT_HEADERS)(pBase+pImgDosHdr->e_lfanew);
+    if (pImgNtHdrs->Signature != IMAGE_NT_SIGNATURE) {
+        printf("[-] Failed getting PIMAGE_NT_HEADERS\n");
+    }
+
+    PIMAGE_EXPORT_DIRECTORY pImgExpDir = (PIMAGE_EXPORT_DIRECTORY)(pBase+pImgNtHdrs->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+
+    DWORD* namesTable = (DWORD*)(pBase+pImgExpDir->AddressOfNames);
+    DWORD* functionsTable = (DWORD*)(pBase+pImgExpDir->AddressOfFunctions);
+    WORD* ordinalsTable = (WORD*)(pBase+pImgExpDir->AddressOfNameOrdinals);
+    
+    for (size_t i=0; i<pImgExpDir->NumberOfFunctions; i++) {
+        char* functionName = (char*)(pBase+namesTable[i]);
+        WORD ordinal = ordinalsTable[i];
+        if (_stricmp(functionName, syscallName) == 0) {
+            printf("[i] %s\n", functionName);
+            // Function Address first bytes would be the syscall stub (assuming it ain't hooked)
+            FARPROC functionAddr = (FARPROC)(pBase+functionsTable[ordinal]);
+
+            // Parse bytes of stub, raw bytes
+            size_t idx = 0;
+            BYTE* bytes = (BYTE*)functionAddr;
+            while (TRUE) {
+                // If function ain't hooked stub will have the following opcodes
+                if (bytes[idx] == 0x4c && bytes[idx+1] == 0x8b && bytes[idx+2] == 0xd1 &&
+                    bytes[idx+3] == 0xb8) {
+                    // The SSN would start at the 5th byte
+                    // We can easily get the value through dereference
+                    DWORD SSN = *(DWORD*)(bytes+idx+4);
+                    printf("\t[+] SSN ENCONTRADO: %d\n", SSN);
+                    return SSN;
+                }
+                idx++;
+                // Sanity break
+                if (idx == 15) return NULL;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    HMODULE hNtdll = CustomGetModuleHandle(L"ntdll.dll");
+    GetSSNByName(hNtdll, "NtQueryInformationProcess");
+    GetSSNByName(hNtdll, "NtAllocateVirtualMemory");
+
+    return 0;
+}
